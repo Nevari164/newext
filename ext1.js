a48429112ed93e92f2f4bfd6e30ffdbfe5116ef9
Nevari164 <85701149+Nevari164@users.noreply.github.com>
2024-07-05 02:54:21 -0400

    Update ext1.js

--- ext1.js
+++ ext1.js
@@ -1,22 +1,38 @@
-class TimeExtension {
-    getInfo() {
-        return {
-            id: 'timeExtension',
-            name: 'Time Extension',
-            blocks: [
-                {
-                    opcode: 'getCurrentTime',
-                    blockType: Scratch.BlockType.REPORTER,
-                    text: 'current time'
+(function (Scratch) {
+    'use strict';
+  
+    // Define the extension metadata
+    const extension = {
+        id: 'randomNumber',
+        name: 'Random Number Generator',
+        blocks: [
+            {
+                opcode: 'getRandomNumber',
+                blockType: Scratch.BlockType.REPORTER,
+                text: 'random number from [MIN] to [MAX]',
+                arguments: {
+                    MIN: {
+                        type: Scratch.ArgumentType.NUMBER,
+                        defaultValue: 1
+                    },
+                    MAX: {
+                        type: Scratch.ArgumentType.NUMBER,
+                        defaultValue: 10
+                    }
                 }
-            ]
-        };
+            }
+        ]
+    };
+
+    // Define the block functionality
+    class RandomNumberExtension {
+        getRandomNumber({ MIN, MAX }) {
+            const min = Math.min(MIN, MAX);
+            const max = Math.max(MIN, MAX);
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
     }
 
-    getCurrentTime() {
-        const date = new Date();
-        return date.toLocaleTimeString();
-    }
-}
-
-Scratch.extensions.register(new TimeExtension());
+    // Register the extension
+    Scratch.extensions.register(new RandomNumberExtension(), extension);
+})(Scratch);
